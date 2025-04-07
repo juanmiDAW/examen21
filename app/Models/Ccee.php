e2a24a3f67c21f5bb3c7617359eaaa62c913e78b
daw <juanmiguel.valle@iesdonana.org>
2025-04-07 20:48:12 +0200

    migraciones, modelos, CRUD

---
+++ app/Models/Ccee.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ccee extends Model
+{
+    /** @use HasFactory<\Database\Factories\CceeFactory> */
+    use HasFactory;
+
+    protected $fillable = ['ce', 'descripcion'];
+
+    public function notas() {
+        $this->hasMany(Nota::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AlumnoController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
+Route::resource('alumnos', AlumnoController::class);
